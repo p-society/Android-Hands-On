7e7ea7d49efee25eb816fcab9ba60ab356601ea4
Shriyasahu2804 <131455350+Shriyasahu2804@users.noreply.github.com>
2023-04-21 20:30:40 +0530

    app done

--- Age-calculator-b222050-shriyasahu/app/src/main/java/com/example/age_calculator_b222050_shriyasahu/MainActivity.kt
+++ Age-calculator-b222050-shriyasahu/app/src/main/java/com/example/age_calculator_b222050_shriyasahu/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.age_calculator_b222050_shriyasahu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        var c = 0
+        var count : TextView = findViewById(R.id.tv_count)
+        var btn : Button = findViewById(R.id.btn_click)
+
+        btn.setOnClickListener {
+            c = c + 1
+            count.text = c.toString()
+        }
+
+   }
 }
