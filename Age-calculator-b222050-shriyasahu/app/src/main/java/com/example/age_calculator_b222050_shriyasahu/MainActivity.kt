d2482f1a31af0dfd77ee6848842a16609831838e
Shriyasahu2804 <131455350+Shriyasahu2804@users.noreply.github.com>
2023-04-21 18:38:52 +0530

    1

---
+++ Age-calculator-b222050-shriyasahu/app/src/main/java/com/example/age_calculator_b222050_shriyasahu/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.age_calculator_b222050_shriyasahu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
