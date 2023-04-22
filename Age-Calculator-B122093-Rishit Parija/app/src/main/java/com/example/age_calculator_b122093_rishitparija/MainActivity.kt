55a856f708566c494a50ccbaa14ef4ef6df0b97e
RishitParija <rishitparija774@gmail.com>
2023-04-22 22:30:47 +0530

    Deleted some files

---
+++ Age-Calculator-B122093-Rishit Parija/app/src/main/java/com/example/age_calculator_b122093_rishitparija/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.age_calculator_b122093_rishitparija
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    var count =0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun onTap(view : View){
+        count++
+        val clickv=findViewById<TextView>(R.id.clickv) as TextView
+        clickv.text="You Have Clicked $count Times"
+    }
+}
